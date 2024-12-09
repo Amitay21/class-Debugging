bfe9732d9c83dc40bd418ee966c3b1461900b599
Amitay21 <amitay0008@gmail.com>
2024-12-09 20:01:25 +0200

    finale i think

---
+++ debugging classWork/DebugWorkshop/Password/Password.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstring>
+
+struct Password
+{
+    char value[16];
+    bool incorrect = true;
+
+    Password() : incorrect(true) {
+        std::memset(value, 0, sizeof(value)); // איפוס המערך
+    }
+};
+
+int main()
+{
+    std::cout << "Enter your password to continue:" << std::endl;
+    Password pwd;
+
+    std::cin.get(pwd.value, sizeof(pwd.value)); // check the length of the arr
+
+    if (std::strcmp(pwd.value, "Amitay") == 0) { // check pass
+        pwd.incorrect = false;
+    }
+
+    if (!pwd.incorrect) {
+        std::cout << "Congratulations\n";
+    }
+
+    return 0;
+}
---
+++ debugging classWork/DebugWorkshop/SafeAndSound/part2.cpp
@@ -0,0 +1,29 @@
+﻿#include "part2.h"
+#include <iostream>
+#include <stdexcept>
+
+char* safe_string_copy(char* dest, unsigned int destsize, const char* src)
+{
+	unsigned int srcsize = (unsigned int)strlen(src);
+	if (srcsize + 1 > destsize) // +1 עבור '\0'
+		throw std::overflow_error("possible buffer overflow");
+
+	char* ret = dest;
+	while ((*dest++ = *src++)); // העתקה כולל '\0'
+	return ret;
+}
+
+
+
+#define BUF_SIZE 20
+void part2()
+{
+	char password[] = "secret";
+	char dest[BUF_SIZE];
+	char src[] = "hello world!";
+
+	safe_string_copy(dest, BUF_SIZE, src);
+
+	std::cout << src << std::endl;
+	std::cout << dest << std::endl;
+}
